e6ee8b1b755832a8aad11cdac400ef17f5667ab0
Torbjörn Kalin <tgithub@kalin-urena.com>
2011-11-07 22:12:51 +0100

    Update assembly information.

--- builder/fs4net.Builder/Properties/AssemblyInfo.cs
+++ builder/fs4net.Builder/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Security.Permissions;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("fs4net.Builder")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Utility for populating a file system with files and directories")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("fs4net.Builder")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
@@ -35,5 +35,5 @@ using System.Security.Permissions;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
