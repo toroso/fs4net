0b26511ae9b69dfc9e4df38f88559e7e2d2b2a71
Torbjörn Kalin <tgithub@kalin-urena.com>
2011-11-19 22:45:52 +0100

    Added documentation.

--- framework/fs4net.Framework/FileSystem.cs
+++ framework/fs4net.Framework/FileSystem.cs
@@ -5,15 +5,26 @@ using fs4net.Framework.Impl;
 
 namespace fs4net.Framework
 {
+    /// <summary>
+    /// A wrapper around the real file system. All file operations done file file descriptors created from this class
+    /// will be done on the real file system.
+    /// </summary>
     public sealed class FileSystem : IInternalFileSystem
     {
         private readonly ILogger _logger;
 
+        /// <summary>
+        /// Instantiate a file system wrapper. The instance is created without a logger which means that all logged
+        /// events are swallowed.
+        /// </summary>
         public FileSystem()
             : this(NullLogger.Instance)
         {
         }
 
+        /// <summary>
+        /// Instantiate a file system wrapper. The instance is created with a logger where logged events are reported.
+        /// </summary>
         /// <param name="logger">Anything worth reporting inside the fs4net classes are sent to this logger instance.</param>
         public FileSystem(ILogger logger)
         {
--- memory/fs4net.Memory/MemoryFileSystem.cs
+++ memory/fs4net.Memory/MemoryFileSystem.cs
@@ -7,6 +7,14 @@ using fs4net.Memory.Node;
 
 namespace fs4net.Memory
 {
+    /// <summary>
+    /// An in-memory file system. All files and directories created are stored in memory. When the Dispose() method is
+    /// called the file system content is erased.
+    /// This file system is supposed to behave exactly as the real file system which means it's a nice thing to use in
+    /// tests.
+    /// To make the file system usable you ought to call the WithDrives() method to configure what drives exists.
+    /// Otherwise the file system has no drives at all.
+    /// </summary>
     public sealed class MemoryFileSystem : IInternalFileSystem, IDisposable
     {
         // TODO: Make this stuff configurable
@@ -43,11 +51,19 @@ namespace fs4net.Memory
         private readonly ILogger _logger;
         private readonly FolderNode _rootNode = FolderNode.CreateRoot();
 
+        /// <summary>
+        /// Instantiate an in-memory file system. The instance is created without a logger which means that all logged
+        /// events are swallowed.
+        /// </summary>
         public MemoryFileSystem()
             : this(NullLogger.Instance)
         {
         }
 
+        /// <summary>
+        /// Instantiate an in-memory file system. The instance is created with a logger where logged events are
+        /// reported.
+        /// </summary>
         /// <param name="logger">Anything worth reporting inside the fs4net classes are sent to this logger instance.</param>
         public MemoryFileSystem(ILogger logger)
         {
@@ -61,8 +77,7 @@ namespace fs4net.Memory
         }
 
         /// <summary>
-        /// Specifies the drives that this FileSystem contains (or actually what additional drives it contains: this
-        /// method does not remove drives that are not specified).
+        /// Specifies the drives that this FileSystem contains. If called several time, the specified drives are appended to the previously specified ones.
         /// The method returns the same instance of the FileSystem.
         /// </summary>
         public MemoryFileSystem WithDrives(params string[] driveNames)
@@ -71,6 +86,9 @@ namespace fs4net.Memory
             return this;
         }
 
+        /// <summary>
+        /// Erases all files and directories in the file system and frees up the memory being used.
+        /// </summary>
         public void Dispose()
         {
             _rootNode.Dispose();
