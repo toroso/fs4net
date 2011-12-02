ca9ec59489eb61ac8345f8a0ccf4335dca6f64a3
Torbjörn Kalin <tgithub@kalin-urena.com>
2011-12-02 20:50:35 +0100

    Move implementation of IInternalFileSystem to separate class to keep public API clean.

--- framework/fs4net.Framework/FileSystem.cs
+++ framework/fs4net.Framework/FileSystem.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using fs4net.Framework.Impl;
 
 namespace fs4net.Framework
@@ -9,9 +8,9 @@ namespace fs4net.Framework
     /// A wrapper around the real file system. All file operations done file file descriptors created from this class
     /// will be done on the real file system.
     /// </summary>
-    public sealed class FileSystem : IInternalFileSystem
+    public sealed class FileSystem : IFileSystem
     {
-        private readonly ILogger _logger;
+        private readonly FileSystemImpl _impl;
 
         /// <summary>
         /// Instantiate a file system wrapper. The instance is created without a logger which means that all logged
@@ -28,181 +27,42 @@ namespace fs4net.Framework
         /// <param name="logger">Anything worth reporting inside the fs4net classes are sent to this logger instance.</param>
         public FileSystem(ILogger logger)
         {
-            _logger = logger;
+            _impl = new FileSystemImpl(logger);
         }
-
-        #region Implementation of IFileSystem
 
         public RootedFile FileDescribing(string fullPath)
         {
-            // TODO: If relative, append it to Current Directory. Or not...?
-            return new RootedFile(this, fullPath, _logger);
+            return _impl.FileDescribing(fullPath);
         }
 
         public RootedDirectory DirectoryDescribing(string fullPath)
         {
-            // TODO: If relative, append it to Current Directory. Or not...?
-            return new RootedDirectory(this, fullPath, _logger);
+            return _impl.DirectoryDescribing(fullPath);
         }
 
         public RootedDirectory DirectoryDescribingTemporaryDirectory()
         {
-            var tempPathWithBackslash = System.IO.Path.GetTempPath();
-            return DirectoryDescribing(tempPathWithBackslash.Remove(tempPathWithBackslash.Length - 1));
+            return _impl.DirectoryDescribingTemporaryDirectory();
         }
 
         public RootedDirectory DirectoryDescribingCurrentDirectory()
         {
-            return DirectoryDescribing(System.IO.Directory.GetCurrentDirectory());
+            return _impl.DirectoryDescribingCurrentDirectory();
         }
 
         public RootedDirectory DirectoryDescribingSpecialFolder(Environment.SpecialFolder folder)
         {
-            if (folder == Environment.SpecialFolder.MyComputer) throw new NotSupportedException("MyComputer cannot be denoted by a RootedDirectory.");
-            return DirectoryDescribing(Environment.GetFolderPath(folder));
+            return _impl.DirectoryDescribingSpecialFolder(folder);
         }
 
         public Drive DriveDescribing(string driveName)
         {
-            return new Drive(this, driveName, _logger);
+            return _impl.DriveDescribing(driveName);
         }
 
         public IEnumerable<Drive> AllDrives()
         {
-            return System.IO.DriveInfo.GetDrives()
-                .Select(driveInfo => DriveDescribing(driveInfo.Name.RemoveTrailingPathSeparators()));
+            return _impl.AllDrives();
         }
-
-        #endregion // Implementation of IFileSystem
-
-
-        #region Implementation of IInternalFileSystem
-
-        public bool IsFile(RootedCanonicalPath path)
-        {
-            return System.IO.File.Exists(path.FullPath);
-        }
-
-        public bool IsDirectory(RootedCanonicalPath path)
-        {
-            return System.IO.Directory.Exists(path.FullPath);
-        }
-
-        public long GetFileSize(RootedCanonicalPath path)
-        {
-            return new System.IO.FileInfo(path.FullPath).Length;
-        }
-
-        public DateTime GetFileLastWriteTime(RootedCanonicalPath path)
-        {
-            return System.IO.File.GetLastWriteTime(path.FullPath);
-        }
-
-        public void SetFileLastWriteTime(RootedCanonicalPath path, DateTime at)
-        {
-            System.IO.File.SetLastWriteTime(path.FullPath, at);
-        }
-
-        public DateTime GetDirectoryLastWriteTime(RootedCanonicalPath path)
-        {
-            return System.IO.Directory.GetLastWriteTime(path.FullPath);
-        }
-
-        public void SetDirectoryLastWriteTime(RootedCanonicalPath path, DateTime at)
-        {
-            System.IO.Directory.SetLastWriteTime(path.FullPath, at);
-        }
-
-        public DateTime GetFileLastAccessTime(RootedCanonicalPath path)
-        {
-            return System.IO.File.GetLastAccessTime(path.FullPath);
-        }
-
-        public void SetFileLastAccessTime(RootedCanonicalPath path, DateTime at)
-        {
-            System.IO.File.SetLastAccessTime(path.FullPath, at);
-        }
-
-        public DateTime GetDirectoryLastAccessTime(RootedCanonicalPath path)
-        {
-            return System.IO.Directory.GetLastAccessTime(path.FullPath);
-        }
-
-        public void SetDirectoryLastAccessTime(RootedCanonicalPath path, DateTime at)
-        {
-            System.IO.Directory.SetLastAccessTime(path.FullPath, at);
-        }
-
-        public IEnumerable<RootedFile> GetFilesInDirectory(RootedCanonicalPath path)
-        {
-            return System.IO.Directory.GetFiles(path.FullPath).Select(filePath => FileDescribing(filePath));
-        }
-
-        public IEnumerable<RootedDirectory> GetDirectoriesInDirectory(RootedCanonicalPath path)
-        {
-            return System.IO.Directory.GetDirectories(path.FullPath).Select(filePath => DirectoryDescribing(filePath));
-        }
-
-        public void CreateDirectory(RootedCanonicalPath path)
-        {
-            System.IO.Directory.CreateDirectory(path.FullPath);
-        }
-
-        public void DeleteFile(RootedCanonicalPath path)
-        {
-            System.IO.File.Delete(path.FullPath);
-        }
-
-        public void DeleteDirectory(RootedCanonicalPath path, bool recursive)
-        {
-            System.IO.Directory.Delete(path.FullPath, recursive);
-        }
-
-        public void MoveFile(RootedCanonicalPath source, RootedCanonicalPath destination)
-        {
-            System.IO.File.Move(source.FullPath, destination.FullPath);
-        }
-
-        public void MoveDirectory(RootedCanonicalPath source, RootedCanonicalPath destination)
-        {
-            System.IO.Directory.Move(source.FullPath, destination.FullPath);
-        }
-
-        public void CopyFile(RootedCanonicalPath source, RootedCanonicalPath destination)
-        {
-            System.IO.File.Copy(source.FullPath, destination.FullPath);
-        }
-
-        public void CopyAndOverwriteFile(RootedCanonicalPath source, RootedCanonicalPath destination)
-        {
-            System.IO.File.Copy(source.FullPath, destination.FullPath, true);
-        }
-
-        public System.IO.Stream CreateReadStream(RootedCanonicalPath path)
-        {
-            return new System.IO.FileInfo(path.FullPath).Open(System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.Read);
-        }
-
-        public System.IO.Stream CreateWriteStream(RootedCanonicalPath path)
-        {
-            return new System.IO.FileInfo(path.FullPath).Open(System.IO.FileMode.Create, System.IO.FileAccess.Write);
-        }
-
-        public System.IO.Stream CreateAppendStream(RootedCanonicalPath path)
-        {
-            return new System.IO.FileStream(path.FullPath, System.IO.FileMode.Append);
-        }
-
-        public System.IO.Stream CreateModifyStream(RootedCanonicalPath path)
-        {
-            return new System.IO.FileInfo(path.FullPath).Open(System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.ReadWrite);
-        }
-
-        public void SetAsCurrent(RootedCanonicalPath path)
-        {
-            System.IO.Directory.SetCurrentDirectory(path.FullPath);
-        }
-
-        #endregion // Implementation of IInternalFileSystem
     }
 }
---
+++ framework/fs4net.Framework/Impl/FileSystemImpl.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace fs4net.Framework.Impl
+{
+    /// <summary>
+    /// Contains IInternalFileSystem implementation of the file system to keep the public interface clean.
+    /// </summary>
+    internal sealed class FileSystemImpl : IInternalFileSystem
+    {
+        private readonly ILogger _logger;
+
+        public FileSystemImpl(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        #region Implementation of IFileSystem
+
+        public RootedFile FileDescribing(string fullPath)
+        {
+            // TODO: If relative, append it to Current Directory. Or not...?
+            return new RootedFile(this, fullPath, _logger);
+        }
+
+        public RootedDirectory DirectoryDescribing(string fullPath)
+        {
+            // TODO: If relative, append it to Current Directory. Or not...?
+            return new RootedDirectory(this, fullPath, _logger);
+        }
+
+        public RootedDirectory DirectoryDescribingTemporaryDirectory()
+        {
+            var tempPathWithBackslash = System.IO.Path.GetTempPath();
+            return DirectoryDescribing(tempPathWithBackslash.Remove(tempPathWithBackslash.Length - 1));
+        }
+
+        public RootedDirectory DirectoryDescribingCurrentDirectory()
+        {
+            return DirectoryDescribing(System.IO.Directory.GetCurrentDirectory());
+        }
+
+        public RootedDirectory DirectoryDescribingSpecialFolder(Environment.SpecialFolder folder)
+        {
+            if (folder == Environment.SpecialFolder.MyComputer) throw new NotSupportedException("MyComputer cannot be denoted by a RootedDirectory.");
+            return DirectoryDescribing(Environment.GetFolderPath(folder));
+        }
+
+        public Drive DriveDescribing(string driveName)
+        {
+            return new Drive(this, driveName, _logger);
+        }
+
+        public IEnumerable<Drive> AllDrives()
+        {
+            return System.IO.DriveInfo.GetDrives()
+                .Select(driveInfo => DriveDescribing(StringExtensions.RemoveTrailingPathSeparators(driveInfo.Name)));
+        }
+
+        #endregion // Implementation of IFileSystem
+
+        #region Implementation of IInternalFileSystem
+
+        public bool IsFile(RootedCanonicalPath path)
+        {
+            return System.IO.File.Exists(path.FullPath);
+        }
+
+        public bool IsDirectory(RootedCanonicalPath path)
+        {
+            return System.IO.Directory.Exists(path.FullPath);
+        }
+
+        public long GetFileSize(RootedCanonicalPath path)
+        {
+            return new System.IO.FileInfo(path.FullPath).Length;
+        }
+
+        public DateTime GetFileLastWriteTime(RootedCanonicalPath path)
+        {
+            return System.IO.File.GetLastWriteTime(path.FullPath);
+        }
+
+        public void SetFileLastWriteTime(RootedCanonicalPath path, DateTime at)
+        {
+            System.IO.File.SetLastWriteTime(path.FullPath, at);
+        }
+
+        public DateTime GetDirectoryLastWriteTime(RootedCanonicalPath path)
+        {
+            return System.IO.Directory.GetLastWriteTime(path.FullPath);
+        }
+
+        public void SetDirectoryLastWriteTime(RootedCanonicalPath path, DateTime at)
+        {
+            System.IO.Directory.SetLastWriteTime(path.FullPath, at);
+        }
+
+        public DateTime GetFileLastAccessTime(RootedCanonicalPath path)
+        {
+            return System.IO.File.GetLastAccessTime(path.FullPath);
+        }
+
+        public void SetFileLastAccessTime(RootedCanonicalPath path, DateTime at)
+        {
+            System.IO.File.SetLastAccessTime(path.FullPath, at);
+        }
+
+        public DateTime GetDirectoryLastAccessTime(RootedCanonicalPath path)
+        {
+            return System.IO.Directory.GetLastAccessTime(path.FullPath);
+        }
+
+        public void SetDirectoryLastAccessTime(RootedCanonicalPath path, DateTime at)
+        {
+            System.IO.Directory.SetLastAccessTime(path.FullPath, at);
+        }
+
+        public IEnumerable<RootedFile> GetFilesInDirectory(RootedCanonicalPath path)
+        {
+            return System.IO.Directory.GetFiles(path.FullPath).Select(FileDescribing);
+        }
+
+        public IEnumerable<RootedDirectory> GetDirectoriesInDirectory(RootedCanonicalPath path)
+        {
+            return System.IO.Directory.GetDirectories(path.FullPath).Select(DirectoryDescribing);
+        }
+
+        public void CreateDirectory(RootedCanonicalPath path)
+        {
+            System.IO.Directory.CreateDirectory(path.FullPath);
+        }
+
+        public void DeleteFile(RootedCanonicalPath path)
+        {
+            System.IO.File.Delete(path.FullPath);
+        }
+
+        public void DeleteDirectory(RootedCanonicalPath path, bool recursive)
+        {
+            System.IO.Directory.Delete(path.FullPath, recursive);
+        }
+
+        public void MoveFile(RootedCanonicalPath source, RootedCanonicalPath destination)
+        {
+            System.IO.File.Move(source.FullPath, destination.FullPath);
+        }
+
+        public void MoveDirectory(RootedCanonicalPath source, RootedCanonicalPath destination)
+        {
+            System.IO.Directory.Move(source.FullPath, destination.FullPath);
+        }
+
+        public void CopyFile(RootedCanonicalPath source, RootedCanonicalPath destination)
+        {
+            System.IO.File.Copy(source.FullPath, destination.FullPath);
+        }
+
+        public void CopyAndOverwriteFile(RootedCanonicalPath source, RootedCanonicalPath destination)
+        {
+            System.IO.File.Copy(source.FullPath, destination.FullPath, true);
+        }
+
+        public System.IO.Stream CreateReadStream(RootedCanonicalPath path)
+        {
+            return new System.IO.FileInfo(path.FullPath).Open(System.IO.FileMode.Open, System.IO.FileAccess.Read, System.IO.FileShare.Read);
+        }
+
+        public System.IO.Stream CreateWriteStream(RootedCanonicalPath path)
+        {
+            return new System.IO.FileInfo(path.FullPath).Open(System.IO.FileMode.Create, System.IO.FileAccess.Write);
+        }
+
+        public System.IO.Stream CreateAppendStream(RootedCanonicalPath path)
+        {
+            return new System.IO.FileStream(path.FullPath, System.IO.FileMode.Append);
+        }
+
+        public System.IO.Stream CreateModifyStream(RootedCanonicalPath path)
+        {
+            return new System.IO.FileInfo(path.FullPath).Open(System.IO.FileMode.OpenOrCreate, System.IO.FileAccess.ReadWrite);
+        }
+
+        public void SetAsCurrent(RootedCanonicalPath path)
+        {
+            System.IO.Directory.SetCurrentDirectory(path.FullPath);
+        }
+
+        #endregion // Implementation of IInternalFileSystem
+    }
+}
